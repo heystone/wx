de41c80153001c7e558c86a6061247b010809712
heystone <shizeping423@163.com>
2018-09-05 19:08:29 +0800

    over

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,15 +13,29 @@ const app = getApp()
 Page({
   data: {
     cards:cards,
-    sel:0
+    sel:-1,
+    a:0,
+    reward:""
   },
   reverseCard:function(event){
     this.setData({
       sel:event.currentTarget.dataset.index
-    })
+    });
+    this.getReward();
   },
   onLoad: function () {
   },
-  initMyLocation:function(){
+  getReward:function(){
+    var a = Math.ceil((Math.random() * 10))-1;
+    var reward="很遗憾没中奖";
+    if(a%3!=0){
+      reward="啪"+a+"次"
+    }
+    var self=this;
+    var index = this.data.sel;
+    this.data.cards[index].back = reward;
+    this.setData({
+       cards:self.data.cards
+    });
   }
 })
